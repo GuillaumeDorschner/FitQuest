75b36a0f91821a321bf1eb71a25aeb73eae0cbcd
Fatima-Zahra7 <83703943+Fatima-Zahra7@users.noreply.github.com>
2023-11-17 23:34:21 +0100

    1e vue du footer/nav

--- app/src/main/java/com/example/workout/MainActivity.kt
+++ app/src/main/java/com/example/workout/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.workout
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageViewHome = findViewById<ImageView>(R.id.ButtonHome)
+        val imageViewActivity = findViewById<ImageView>(R.id.ButtonActivity)
+        val imageViewNewActivity = findViewById<ImageView>(R.id.ButtonNewActivity)
+        val imageViewFavoris = findViewById<ImageView>(R.id.ButtonFavoris)
+        val imageViewProfil = findViewById<ImageView>(R.id.ButtonProfil)
     }
 }
