a6ff7e4f330c52293090ff1c25de8621bcc0d3a6
Guillaume Dorschner <Guillaume.dorschner@gmail.com>
2023-11-19 00:23:14 +0100

    dodo

--- app/src/main/java/com/example/workout/fragment_page_new_workout.kt
+++ app/src/main/java/com/example/workout/fragment_page_new_workout.kt
@@ -5,55 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.workout.databinding.FragmentPageNewWorkoutBinding
+import com.example.workout.viewmodel.WorkoutViewModel
+import com.example.workout.model.CurrentOpenIARequest
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [fragment_page_new_workout.newInstance] factory method to
- * create an instance of this fragment.
- */
 class fragment_page_new_workout : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentPageNewWorkoutBinding
+    private lateinit var viewModel: WorkoutViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_page_new_workout, container, false)
+        viewModel = ViewModelProvider(this).get(WorkoutViewModel::class.java)
+        binding.workoutViewModel = viewModel
+
+        binding.buttonRun.setOnClickListener {
+            val userInput = binding.editTextWorkout.text.toString()
+
+            val systemMessage = MessagesItem(
+                role = "system",
+                content = "You are a fitness assistant, skilled in creating personalized workout programs. " +
+                        "Create a programme perfectly suited to the user's level {beginner, intermediate, expert}. " +
+                        "Choose the number of training days per week. Focus on these body parts/muscles: {user message}. " +
+                        "If the target has nothing to do with bodybuilding, reply: I'm afraid I can't help you with that. " +
+                        "Let's stay focused on bodybuilding."
+            )
+
+            val requestData = CurrentOpenIARequest(
+                model = "gpt-4",
+                messages = listOf(systemMessage, MessagesItem(role = "user", content = userInput))
+            )
+
+            viewModel.postWorkoutQuery(requestData)
         }
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page_new_workout, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_page_new_workout.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            fragment_page_new_workout().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        return binding.root
     }
 }
