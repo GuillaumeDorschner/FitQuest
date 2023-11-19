2780a9280e6e8f9c4805765c61776c253e5321d3
Guillaume Dorschner <Guillaume.dorschner@gmail.com>
2023-11-19 21:24:09 +0100

    open the page and scroll view added

--- app/src/main/java/com/example/workout/fragments/New_Workout.kt
+++ app/src/main/java/com/example/workout/fragments/New_Workout.kt
@@ -17,44 +17,56 @@ class New_Workout : Fragment() {
     private lateinit var binding: FragmentNewWorkoutBinding
     private lateinit var viewModel: WorkoutViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_new_workout, container, false)
-        viewModel = ViewModelProvider(this)[WorkoutViewModel::class.java]
-        binding.workoutViewModel = viewModel
-
-        binding.searchIcon.setOnClickListener {
-            val userInput = binding.inputUsr.text.toString()
-
-            val systemMessage = MessagesItem(
-                role = "system",
-                content = "You are a fitness assistant, skilled in creating personalized workout programs. " +
-                        "Create a programme perfectly suited to the user's level {beginner, intermediate, expert}. " +
-                        "Choose the number of training days per week. Focus on these body parts/muscles: {user message}. " +
-                        "If the target has nothing to do with bodybuilding, reply: I'm afraid I can't help you with that. " +
-                        "Let's stay focused on bodybuilding."
-            )
-
-            val requestData = CurrentOpenIARequest(
-                model = "gpt-4",
-                messages = listOf(systemMessage, MessagesItem(role = "user", content = userInput))
-            )
-
-            viewModel.postWorkoutQuery(requestData)
-        }
-
-        setupObservers()
-
-        return binding.root
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
-    private fun setupObservers() {
-        viewModel.workoutResponse.observe(viewLifecycleOwner, { response ->
-            val content = response?.choices?.firstOrNull()?.message?.content
-            binding.response.text = content
-        })
-
-        viewModel.errorMessage.observe(viewLifecycleOwner, { error ->
-            // Handle the error message here, e.g., display a Toast
-        })
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_workout, container, false)
     }
+
+    // override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    //     binding = DataBindingUtil.inflate(inflater, R.layout.fragment_new_workout, container, false)
+    //     viewModel = ViewModelProvider(this)[WorkoutViewModel::class.java]
+    //     binding.workoutViewModel = viewModel
+
+    //     binding.searchIcon.setOnClickListener {
+    //         val userInput = binding.inputUsr.text.toString()
+
+    //         val systemMessage = MessagesItem(
+    //             role = "system",
+    //             content = "You are a fitness assistant, skilled in creating personalized workout programs. " +
+    //                     "Create a programme perfectly suited to the user's level {beginner, intermediate, expert}. " +
+    //                     "Choose the number of training days per week. Focus on these body parts/muscles: {user message}. " +
+    //                     "If the target has nothing to do with bodybuilding, reply: I'm afraid I can't help you with that. " +
+    //                     "Let's stay focused on bodybuilding."
+    //         )
+
+    //         val requestData = CurrentOpenIARequest(
+    //             model = "gpt-4",
+    //             messages = listOf(systemMessage, MessagesItem(role = "user", content = userInput))
+    //         )
+
+    //         // viewModel.postWorkoutQuery(requestData)
+    //     }
+
+    //     setupObservers()
+
+    //     return binding.root
+    // }
+
+    // private fun setupObservers() {
+    //     viewModel.workoutResponse.observe(viewLifecycleOwner, { response ->
+    //         val content = response?.choices?.firstOrNull()?.message?.content
+    //         binding.response.text = content
+    //     })
+
+    //     viewModel.errorMessage.observe(viewLifecycleOwner, { error ->
+    //         // Handle the error message here, e.g., display a Toast
+    //     })
+    // }
 }
