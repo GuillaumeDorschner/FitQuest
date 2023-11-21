01903f9f3749c240ca03ef6031cce4f4f61bd1dc
Guillaume Dorschner <Guillaume.dorschner@gmail.com>
2023-11-21 14:51:46 +0100

    reponse (content) show to user

--- app/src/main/java/com/example/workout/fragments/New_Workout.kt
+++ app/src/main/java/com/example/workout/fragments/New_Workout.kt
@@ -15,7 +15,6 @@ import com.example.workout.networking.ApiConfig
 import com.example.workout.viewmodel.WorkoutViewModel
 import com.example.workout.viewmodel.WorkoutViewModelFactory
 
-
 class New_Workout : Fragment() {
     private lateinit var binding: FragmentNewWorkoutBinding
     private lateinit var viewModel: WorkoutViewModel
@@ -25,33 +24,41 @@ class New_Workout : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        // Initialize ApiService
-        val apiService = ApiConfig.getApiService()
-
-        // Initialize ViewModel with the Factory
-        viewModel = ViewModelProvider(this, WorkoutViewModelFactory(apiService)).get(WorkoutViewModel::class.java)
-
+        // Initialize ViewModel
+        viewModel = ViewModelProvider(this, WorkoutViewModelFactory(ApiConfig.getApiService())).get(WorkoutViewModel::class.java)
 
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_new_workout, container, false)
-        viewModel = ViewModelProvider(this)[WorkoutViewModel::class.java]
         binding.workoutViewModel = viewModel
 
+        // Setup button click listener
         binding.searchButton.setOnClickListener {
             val userInput = binding.inputUsr.text.toString()
-
-            val systemMessage = MessagesItem(
-                role = "system",
-                content = "You are a fitness assistant, skilled in creating personalized workout programs. Create a programme perfectly suited to the user's level {beginner, intermediate, exper}. Choose the number of training days per week. Focus on these body parts/muscles: {user message}. If the target has nothing to do with bodybuilding, reply: I'm afraid I can't help you with that. Let's stay focused on bodybuilding."
-            )
-            val userMessage = MessagesItem(role = "user", content = userInput)
-
             val request = CurrentOpenIARequest(
                 model = "gpt-4",
-                messages = listOf(systemMessage, userMessage)
+                messages = listOf(
+                    MessagesItem(role = "system", content = "Your system message"),
+                    MessagesItem(role = "user", content = userInput)
+                )
             )
-
             viewModel.postWorkoutQuery(request)
+        }
+
+        // Observe workoutResponse
+        viewModel.workoutResponse.observe(viewLifecycleOwner) { response ->
+
+            if (response != null && response.choices?.isNotEmpty() == true) {
+                val content = response.choices?.get(0)?.message?.content
+                binding.response.text = content
+            }
+
+        }
+
+        // Observe errorMessage
+        viewModel.errorMessage.observe(viewLifecycleOwner) { error ->
+            if (error != null) {
+                binding.response.text = "An error occurred retry"
+            }
         }
 
         return binding.root
