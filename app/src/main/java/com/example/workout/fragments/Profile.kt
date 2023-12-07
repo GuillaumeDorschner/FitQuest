0e2ecd2dc632ffa136b6d88656eb17871c0e0396
Guillaume Dorschner <Guillaume.dorschner@gmail.com>
2023-12-07 23:19:14 +0100

    add the page / fragment

---
+++ app/src/main/java/com/example/workout/fragments/Profile.kt
@@ -0,0 +1,28 @@
+package com.example.workout.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.workout.R
+import com.example.workout.databinding.FragmentProfileBinding
+
+class Profile : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
