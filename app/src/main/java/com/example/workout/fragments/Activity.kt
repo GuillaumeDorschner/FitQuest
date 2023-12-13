8ccca9f281ec7a58d51b61047860c7c1145a6a27
Guillaume Dorschner <guillaume.dorschner@icloud.com>
2023-12-13 16:50:58 +0100

    fix: graph

--- app/src/main/java/com/example/workout/fragments/Activity.kt
+++ app/src/main/java/com/example/workout/fragments/Activity.kt
@@ -1,9 +1,11 @@
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.db.williamchart.view.BarChartView
+import com.db.williamchart.view.DonutChartView
 import com.db.williamchart.view.LineChartView
 import com.example.workout.R
 
@@ -54,7 +56,29 @@ class Activity : Fragment() {
             Pair(day, hours / maxTrainingTime) // Normalize the hours to the scale of 0.0 - 1.0 based on the maxTrainingTime.
         }
 
+        // Configurez vos DonutChartViews
+        val chartDonut1 = view.findViewById<DonutChartView>(R.id.chartDonut1)
+        val chartDonut2 = view.findViewById<DonutChartView>(R.id.chartDonut2)
+        val chartDonut3 = view.findViewById<DonutChartView>(R.id.chartDonut3)
 
+        // Définissez les données pour chaque DonutChart
+        val donutData1 = listOf(50F, 50F) // Exemple de données pour chartDonut1
+        val donutData2 = listOf(30F, 70F) // Exemple de données pour chartDonut2
+        val donutData3 = listOf(80F, 20F) // Exemple de données pour chartDonut3
+
+        // Configurez et animez chaque DonutChart
+        val colors = intArrayOf(Color.parseColor("#9FEC00"), Color.parseColor("#D9D9D9"))
+        chartDonut1.donutColors = colors
+        chartDonut1.animation.duration = 1000L
+        chartDonut1.animate(donutData1)
+
+        chartDonut2.donutColors = colors
+        chartDonut2.animation.duration = 1000L
+        chartDonut2.animate(donutData2)
+
+        chartDonut3.donutColors = colors
+        chartDonut3.animation.duration = 1000L
+        chartDonut3.animate(donutData3)
 
         // Show the normalized chart data.
         chart.show(normalizedChartData)
